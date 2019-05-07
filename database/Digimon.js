26fd472508c6f5d5923c3f180fda5a5357c504ad
Shadow Smith <hey@shadowsmith.com>
2019-05-07 15:40:23 -0500

    Added more Digimon

--- database/Digimon.js
+++ database/Digimon.js
@@ -69,6 +69,186 @@ const digimon = [
         id: 14,
         name: "Patamon",
         level: "Rookie"
+    },
+    {
+        id: 15,
+        name: "Kuwagamon",
+        level: "Champion"
+    },
+    {
+        id: 16,
+        name: "Greymon",
+        level: "Champion"
+    },
+    {
+        id: 17,
+        name: "Shellmon",
+        level: "Champion"
+    },
+    {
+        id: 18,
+        name: "Garurumon",
+        level: "Champion"
+    },
+    {
+        id: 19,
+        name: "Seadramon",
+        level: "Champion"
+    },
+    {
+        id: 20,
+        name: "Monochromon",
+        level: "Champion"
+    },
+    {
+        id: 21,
+        name: "Birdramon",
+        level: "Champion"
+    },
+    {
+        id: 22,
+        name: "Meramon",
+        level: "Champion"
+    },
+    {
+        id: 23,
+        name: "Kabuterimon",
+        level: "Champion"
+    },
+    {
+        id: 24,
+        name: "Andromon",
+        level: "Ultimate"
+    },
+    {
+        id: 25,
+        name: "Togemon",
+        level: "Champion"
+    },
+    {
+        id: 26,
+        name: "Numemon",
+        level: "Champion"
+    },
+    {
+        id: 27,
+        name: "Monzaemon",
+        level: "Ultimate"
+    },
+    {
+        id: 28,
+        name: "Ikkakumon",
+        level: "Champion"
+    },
+    {
+        id: 29,
+        name: "Unimon",
+        level: "Champion"
+    },
+    {
+        id: 30,
+        name: "Leomon",
+        level: "Champion"
+    },
+    {
+        id: 31,
+        name: "Ogremon",
+        level: "Champion"
+    },
+    {
+        id: 32,
+        name: "Devimon",
+        level: "Champion"
+    },
+    {
+        id: 33,
+        name: "Frigimon",
+        level: "Champion"
+    },
+    {
+        id: 34,
+        name: "Mojyamon",
+        level: "Champion"
+    },
+    {
+        id: 35,
+        name: "Sukamon",
+        level: "Champion"
+    },
+    {
+        id: 36,
+        name: "Centarumon",
+        level: "Champion"
+    },
+    {
+        id: 37,
+        name: "Bakemon",
+        level: "Champion"
+    },
+    {
+        id: 38,
+        name: "Elecmon",
+        level: "Rookie"
+    },
+    {
+        id: 39,
+        name: "Botamon",
+        level: "Rookie"
+    },
+    {
+        id: 40,
+        name: "Punimon",
+        level: "In Training"
+    },
+    {
+        id: 41,
+        name: "Poyomon",
+        level: "In Training"
+    },
+    {
+        id: 42,
+        name: "Yuramon",
+        level: "In Training"
+    },
+    {
+        id: 43,
+        name: "Angemon",
+        level: "Champion"
+    },
+    {
+        id: 44,
+        name: "Whamon",
+        level: "Champion"
+    },
+    {
+        id: 45,
+        name: "Drimogemon",
+        level: "Champion"
+    },
+    {
+        id: 46,
+        name: "Etemon",
+        level: "Ultimate"
+    },
+    {
+        id: 47,
+        name: "Gazimon",
+        level: "Rookie"
+    },
+    {
+        id: 48,
+        name: "Pagumon",
+        level: "In Training"
+    },
+    {
+        id: 49,
+        name: "SkullGreymon",
+        level: "Ultimate"
+    },
+    {
+        id: 50,
+        name: "Kokatorimon",
+        level: "Champion"
     }
 ];
 
