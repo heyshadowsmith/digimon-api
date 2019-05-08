2c0f7303977d13ca9b602a49fb305f27cab1e747
Shadow Smith <hey@shadowsmith.com>
2019-05-08 11:50:58 -0500

    Add playground and favicon

--- public/main.js
+++ public/main.js
@@ -1,24 +1,38 @@
-const request = new XMLHttpRequest();
-
-request.open("GET", "https://digimon-api.herokuapp.com", true);
-
-request.onload = function() {
-    
-    const data = JSON.parse(this.response);
-    
-    if (request.status >= 200 && request.status < 400) {
-        console.log(data);
-    } else {
-        console.log('error');
-    }
-    
-};
-
-request.send();
-
 let vm = new Vue({
     el: "#app",
     data: {
-        name: "Shadow"
+        id: "",
+        name: "",
+        level: "",
+        url: "",
+        response: "",
+        endpoint: "https://digimon-api.herokuapp.com/api/digimon/"
+    },
+    methods: {
+        searchName: function() {
+            let name = "name/" + this.name;
+            this.getDigimon(name);  
+            this.name = "";
+        },
+        searchLevel: function() {
+            let level = "level/" + this.level;
+            this.getDigimon(level);
+            this.level = "";
+        },
+        searchId: function() {
+            let id = "id/" + this.id;
+            this.getDigimon(id);
+            this.id = "";
+        },
+        getDigimon: function(parameters) {
+            axios({
+                method: "GET",
+                "url": this.endpoint + parameters
+            }).then(result => {
+                this.response = result.data;
+            }, error => {
+                console.error(error);
+            });
+        }
     }
 });
